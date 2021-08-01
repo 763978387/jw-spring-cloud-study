adb8c249695e327c0b4638ce280093e627fc6e86
JiangWei <33048553+763978387@users.noreply.github.com>
2021-08-01 12:25:16 +0800

    nacos study add
    提交nacos的学习

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/nacosconfigcenter/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package nacosconfigcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+/**
+ * @author jiangwei
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
+
