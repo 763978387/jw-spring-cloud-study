3b36b0d9518153529f5c8d657e35604b9e172bdd
JiangWei <33048553+763978387@users.noreply.github.com>
2021-08-03 23:13:26 +0800

    增加了对Sentinel的支持，练习了Sentinel的各种流控和降级熔断规则-2

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/order/OrderNacosMain84.java
@@ -0,0 +1,17 @@
+package order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+//@EnableFeignClients
+public class OrderNacosMain84 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain84.class, args);
+    }
+}
+
+
